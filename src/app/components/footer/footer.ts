de5a73a2ca561cc351073f636f07578191f18ac7
Metalderly2023 <osvaldoeverdiaz@gmail.com>
2026-06-15 03:31:52 -0300

    hasta el momento al parecer esta terminado

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LanguageService } from '../../services/language';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.css',
+  styleUrl: './footer.css'
 })
-export class Footer {}
+export class Footer {
+  lang = inject(LanguageService);
+}
