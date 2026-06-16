800de458890d23d50f00b12064c064a429a388b2
Metalderly2023 <osvaldoeverdiaz@gmail.com>
2026-06-16 17:32:48 -0300

    arreglando errores que dio en versel

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCacRTb1upKJNZhfwRBl2AJEC937BlqfYE",
+        authDomain: "portfolio-metal-37be8.firebaseapp.com",
+        projectId: "portfolio-metal-37be8",
+        storageBucket: "portfolio-metal-37be8.firebasestorage.app",
+        messagingSenderId: "377417720680",
+        appId: "1:377417720680:web:8f1fcfb0d17b62b849acdd",
+        measurementId: "G-0NGMF9EQWD"
+    }
+};
